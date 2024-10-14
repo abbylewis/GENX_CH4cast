5e1514f6d22d1172ea51e595836cc3fd57097c21
abbylewis <aslewis@vt.edu>
2024-10-14 12:39:04 -0400

    re-processed all

--- R/download_data.R
+++ R/download_data.R
@@ -6,7 +6,9 @@ library(tidyverse)
 #Identify all files
 files <- drop_dir(path = "GCREW_LOGGERNET_DATA")
 relevant_files <- files %>%
-  filter(grepl("GENX_INSTRUMENT_FLUX_COMB", name))
+  filter(grepl("GENX_INSTRUMENT_FLUX_COMB", name) |
+           grepl("GENX_FLUX_", name) |
+           grepl("GENX_LGR_FLUX_", name))
 current <- drop_dir(path = "GCREW_LOGGERNET_DATA/current_data") %>%
   filter(grepl("GENX_INSTRUMENT_FLUX_COMB", name),
          !grepl("backup", name))
@@ -47,14 +49,20 @@ if(nrow(relevant_files) == 0){
     map(load_file)
 }
 
-data <- list.files("./Raw_data/dropbox_downloads", full.names = T) %>%
-  map(read_csv, show_col_types = F, skip = 1) %>%
+data_raw <- list.files("./Raw_data/dropbox_downloads", full.names = T) %>%
+  map(read_csv, col_types = cols(.default = "c"), skip = 1)  %>%
   bind_rows() %>%
-  filter(!is.na(TIMESTAMP),
-         !TIMESTAMP == "TS") %>%
+  filter(!TIMESTAMP == "TS") %>%
   mutate(TIMESTAMP = as_datetime(TIMESTAMP)) %>%
-  distinct() %>%
-  filter(!duplicated(LGR_Time)) #I've spent some time looking into this. CH4 is also duplicated for these
+  filter(!is.na(TIMESTAMP)) %>%
+  distinct() 
+data <- data_raw %>%
+  filter(is.na(LGR_Time) |
+           !duplicated(LGR_Time) | 
+           !duplicated(CH4_ppm)) #I've spent some time looking into this and there are some duplicated LGR rows
+data_small <- data %>%
+  select(TIMESTAMP, CH4_ppm, CO2_ppm, MIU_VALVE, GasT_C)
+rm(data)
 
 #Create function to assign groups for separate readings
 group_fun <- function(MIU_VALVE) {
@@ -72,15 +80,20 @@ group_fun <- function(MIU_VALVE) {
 ### Calculate slopes ###
 #Prep data
 index_cutoff <- 30 #Higher would be better, but in the summer we wouldn't have enough data to calculate fluxes
-filtered_data <- data %>%
-  filter(!MIU_VALVE == 16) %>%
+filtered_data <- data_small %>%
+  mutate(CH4_ppm = as.numeric(CH4_ppm),
+         CO2_ppm = as.numeric(CO2_ppm),
+         GasT_C = as.numeric(GasT_C),
+         MIU_VALVE = as.numeric(MIU_VALVE)) %>%
+  filter(!MIU_VALVE == 16,
+         !is.na(MIU_VALVE)) %>%
   arrange(TIMESTAMP) %>%
   mutate(group = group_fun(MIU_VALVE)) %>%
   group_by(group, MIU_VALVE) %>%
   mutate(start = min(TIMESTAMP),
          change = as.numeric(difftime(TIMESTAMP, start, units = "days")),
          index = row_number(),
-         n = sum(index > index_cutoff)) %>%
+         n = sum(index > index_cutoff & !is.na(CH4_ppm))) %>%
   filter(index > index_cutoff,
          n >= 5, #need at least 5 data points to calculate slope
          n < 75 #probably some issue if this many measurements are taken
@@ -118,33 +131,37 @@ fit_cutoff <- quantile(slopes$CH4_rmse, 0.99)
 #These poor fits have extreme values
 slopes %>%
   ggplot(aes(x = CH4_init, y = CH4_slope_umol_per_day, color = CH4_rmse > fit_cutoff)) +
-  geom_point()
+  geom_point() +
+  geom_vline(xintercept = 1.5) +
+  geom_vline(xintercept = 2.5)
 
 slopes_clean <- slopes %>%
   filter(CH4_rmse < fit_cutoff,
-         CH4_init > 1,
+         CH4_init > 1.5,
          CH4_init < 2.5)
 
 #R2 is not a good way to filter because this preferentially removes values near 0
 slopes_clean %>%
   ggplot(aes(x = CH4_init, y = CH4_slope_umol_per_day, color = CH4_R2)) +
-  geom_point() +
-  geom_smooth()
+  geom_point()
 
 #High p-values are all close to 0 (good)
 slopes_clean %>%
   ggplot(aes(x = CH4_init, y = CH4_slope_umol_per_day, color = CH4_p)) +
-  geom_point() +
-  geom_smooth()
+  geom_point() 
 
 metadata <- read_csv("Raw_data/chamber_metadata.csv", show_col_types = F)
 slopes_metadata <- metadata %>%
-  left_join(slopes_clean, by = c("miu_valve" = "MIU_VALVE")) %>%
-  rename(time2 = TIMESTAMP)
+  left_join(slopes_clean %>%
+              mutate(MIU_VALVE = as.numeric(MIU_VALVE)), 
+            by = c("miu_valve" = "MIU_VALVE")) %>%
+  rename(time2 = TIMESTAMP) %>%
+  filter(year(time2) >= 2021)
 
-write.csv(slopes_metadata, "L1_2024.csv", row.names = FALSE)
+write.csv(slopes_metadata, "L1.csv", row.names = FALSE)
 
 slopes_clean %>%
+  filter(year(TIMESTAMP) >= 2021) %>%
   ggplot(aes(x = TIMESTAMP, y = CH4_slope_umol_per_day)) +
   geom_point() +
   facet_wrap(~MIU_VALVE)
