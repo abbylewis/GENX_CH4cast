bc9f6e5c33e8c4603cef56064031b35fd8ec7a7f
abbylewis <aslewis@vt.edu>
2024-11-20 09:15:50 -0500

    read_url

--- R/get_historical_weather.R
+++ R/get_historical_weather.R
@@ -31,3 +31,19 @@ get_historical_weather <- function (latitude, longitude, site_id = NULL, start_d
                                                                                                        "variable", "prediction", "unit"))
   return(df)
 }
+
+read_url <- function(url_base, url_path){
+  out <- tryCatch({
+    out <- httr2::request(url_base) |>
+      httr2::req_url_path_append(url_path) |>
+      httr2::req_throttle(10 / 60, realm = url_base) |>
+      httr2::req_perform() |>
+      httr2::resp_body_json(simplifyVector = TRUE)
+  }, error = function(err) {
+    out <- jsonlite::read_json(file.path(url_base,url_path), simplifyVector = TRUE)
+    return(out)
+  }, finally = {
+    NULL
+  })
+  
+}
