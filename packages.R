e0a56d5f5ed27c1ea3567b4afd354e40a0a15593
abbylewis <aslewis@vt.edu>
2024-12-02 08:08:23 -0500

    skirt the problem with rocker

--- packages.R
+++ packages.R
@@ -8,5 +8,3 @@ install.packages("fabletools")
 install.packages("fable")
 install.packages("slider")
 install.packages("rEDM")
-sf <- "https://cran.r-project.org/src/contrib/Archive/sf/sf_0.9-0.tar.gz"
-install.packages(sf, repos=NULL, type = "source")
