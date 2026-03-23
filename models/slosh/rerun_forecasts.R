c398f6cfff1fed18143b78caaf0cba8e529be552
abbylewis <aslewis@vt.edu>
2026-03-23 12:39:54 -0400

    slosh reruns

--- models/slosh/rerun_forecasts.R
+++ models/slosh/rerun_forecasts.R
@@ -2,7 +2,7 @@ source("./models/slosh/forecast_model.R")
 source("./R/rerun_forecasts.R")
 source("./R/generate_tg_forecast.R")
 
-END <- as_date('2025-10-15') # Don't re-run if forecasts have been submitted after this date
+END <- as_date('2026-03-23') # Don't re-run if forecasts have been submitted after this date
 
 rerun_forecasts(forecast_model = forecast_model,
                 model_variables = model_variables,
