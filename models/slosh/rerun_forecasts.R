5356292ba189f91fe64062c6d7c7c2365b7f5c54
abbylewis <aslewis@vt.edu>
2026-09-15 23:22:57 -0400

    fix forecast and start rerunning

--- models/slosh/rerun_forecasts.R
+++ models/slosh/rerun_forecasts.R
@@ -2,7 +2,7 @@ source("./models/slosh/forecast_model.R")
 source("./R/rerun_forecasts.R")
 source("./R/generate_tg_forecast.R")
 
-END <- as_date('2026-07-29') # Don't re-run if forecasts have been submitted on/after this date
+END <- as_date('2026-07-28') # Don't re-run if forecasts have been submitted on/after this date
 
 rerun_forecasts(forecast_model = forecast_model,
                 model_variables = model_variables,
@@ -12,4 +12,4 @@ rerun_forecasts(forecast_model = forecast_model,
                 noaa = noaa,
                 END = END,
                 start_date = '2025-04-01',
-                end_date = '2026-07-28')
+                end_date = Sys.Date())
